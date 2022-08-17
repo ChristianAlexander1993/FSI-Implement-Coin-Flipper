df670469d764963dfaa569a79932a2dd10febad1
ChristianAlexander1993 <christian.alexandergit@gmail.com>
2022-08-16 21:44:20 -0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // TODO: Declare any global variables we need
-
+var heads = 0
+var percentageOfHeads = 0
+var tails = 0
+var percentageOfTails = 0
+var pennyImage = document.getElementById('penny-image')
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
@@ -7,10 +11,38 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    let flipButton = document.querySelector('#flip') 
+    flipButton.addEventListener('click', flipCoin);
+        function flipCoin(){
+        let flip = Math.floor(Math.random() * 2);
+        if (flip === 0){
+            heads+
+            console.log("heads" + heads);
+            pennyImage.setAttribute('src', './assets/images/penny-heads.jpg')
+            document.getElementById('message').innerHTML = "It's Heads!"
+            document.getElementById('heads').innerHTML = heads++
+        }
+        else {
+            if (flip === 1){
+                tails+
+                console.log("tails" + tails);
+                pennyImage.setAttribute('src', './assets/images/penny-tails.jpg')
+                document.querySelector('#message').innerHTML = "It's Tails!"
+                document.getElementById('tails').innerHTML = tails++
+            }
+        }
+    }
+
+    let clearButton = document.querySelector('#clear');
+    clearButton.addEventListener('click', clearBoard);
+    
+
+    
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
+        
 
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
